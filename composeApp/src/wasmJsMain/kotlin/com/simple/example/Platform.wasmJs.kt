ff7feccd2e700cd1032824b0179c81a7bade58af
Pavel Shishkin <pavel.shishkin@jetbrains.com>
2025-02-10 20:10:30 +0100

    init

---
+++ composeApp/src/wasmJsMain/kotlin/com/simple/example/Platform.wasmJs.kt
@@ -0,0 +1,7 @@
+package com.simple.example
+
+class WasmPlatform: Platform {
+    override val name: String = "Web with Kotlin/Wasm"
+}
+
+actual fun getPlatform(): Platform = WasmPlatform()
---
+++ settings.gradle.kts
@@ -0,0 +1,35 @@
+rootProject.name = "KotlinProject"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("/Users/pavel.shishkin/Source/kotlin/build/repo")
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("/Users/pavel.shishkin/Source/kotlin/build/repo")
+    }
+}
+
+include(":composeApp")

34b93576c618beca4b3ced0d2077c1e7a86bf117
Pavel Shishkin <pavel.shishkin@jetbrains.com>
2025-06-26 15:31:58 +0200

    enable android library for lib project

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,19 +1,20 @@
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-//    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.androidLibrary)
 }
 
 kotlin {
-//    androidTarget {
-//        compilerOptions {
-//            jvmTarget.set(JvmTarget.JVM_11)
-//        }
-//    }
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
 
     listOf(
         iosX64(),
@@ -60,12 +61,12 @@ kotlin {
     }
 }
 
-//android {
-//    namespace = "com.simple.example.lib"
-//    compileSdk = libs.versions.android.compileSdk.get().toInt()
-//
+android {
+    namespace = "org.example.project.lib"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
 //    defaultConfig {
-//        applicationId = "com.simple.example.lib"
+//        applicationId = "org.example.project.lib"
 //        minSdk = libs.versions.android.minSdk.get().toInt()
 //        targetSdk = libs.versions.android.targetSdk.get().toInt()
 //        versionCode = 1
@@ -85,8 +86,4 @@ kotlin {
 //        sourceCompatibility = JavaVersion.VERSION_11
 //        targetCompatibility = JavaVersion.VERSION_11
 //    }
-//}
-
-//dependencies {
-//    debugImplementation(compose.uiTooling)
-//}
+}
